8a7a2fc23760d00549e6d9e883fed0fb5888ceed
Jimena6817 <jimenagobo@gmail.com>
2025-06-26 14:20:39 -0600

    Add files via upload

---
+++ layout.tsx
@@ -0,0 +1,9 @@
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="es">
+      <body style={{ margin: 0, fontFamily: 'sans-serif' }}>
+        {children}
+      </body>
+    </html>
+  );
+}
